f88831f63aa5be56ed9514b42e8abe02b37de266
Alexandra Min <alexandramin@dhcp-10-250-210-70.harvard.edu>
2020-11-04 20:18:01 -0500

    dd spider
    Add spider

--- crimson/crimson/spiders/ArticleItem.py
+++
@@ -1,9 +0,0 @@
-import scrapy
-from scrapy.loader.processors import MapCompose
-from w3lib.html import remove_tags
-
-
-class ArticleItem(scrapy.Item):
-    title = scrapy.Field(input_processor=MapCompose(remove_tags))
-    author = scrapy.Field(input_processor=MapCompose(remove_tags))
-    body = scrapy.Field(input_processor=MapCompose(remove_tags))
